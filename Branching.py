90561f2dba3923d8f282fcb29fbce04da62bf398
sameerafkhan <89035658+sameerafkhan@users.noreply.github.com>
2022-07-05 12:43:48 -0500

    Add more elif statements

--- Branching.py
+++ Branching.py
@@ -2,7 +2,7 @@ flavor_icecream = input('Enter a flavor: ')
 Topping = input('Enter a topping: ')
 cup_size = input('Enter a cup size: ')
 
-if flavor_icecream == 'Vanilla':
+if flavor_icecream == 'vanilla':
     Topping == 'chocolate chips'
     cup_size == 'large'
     print('Enjoy your', flavor_icecream, 'Icecream')
@@ -14,6 +14,10 @@ elif flavor_icecream == 'strawberry':
     Topping == 'almond'
     cup_size == 'small'
     print('Enjoy your', flavor_icecream, 'Icecream')
+elif flavor_icecream == 'mango':
+    Topping == 'pistachios'
+    cup_size == 'large'
+    print('Enjoy our seasonal', flavor_icecream, 'icecrame flavor')
 else:
     print('Have a nice day!')
 
